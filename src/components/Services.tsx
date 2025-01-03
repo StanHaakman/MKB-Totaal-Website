08435a7be92b9958ff51bf497efbf5de682794a9
Staal Ai <stan@staal-ai.nl>
2025-01-03 19:25:04 +0100

    update text light mode

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -33,10 +33,10 @@ export default function Services() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
             <h2 className="text-base/7 font-semibold text-primary-light">Wat wij doen</h2>
-            <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl lg:text-balance">
+            <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-800 dark:text-white sm:text-5xl lg:text-balance">
               Praktische oplossingen voor uw bedrijf
             </p>
-            <p className="mt-6 text-lg/6 text-white">
+            <p className="mt-6 text-lg/6 text-gray-800 dark:text-white">
               Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
               pulvinar et feugiat blandit at. In mi viverra elit nunc.
             </p>
